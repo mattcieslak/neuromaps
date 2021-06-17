01d640131b1461cbc210f008050421cabd79c19a
rmarkello <rmarkello@users.noreply.github.com>
2021-06-17 19:36:39 +0000

    [TEST] Fix doc-test 4d286ff61ad8b34aee01a55d9011df2023112714

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g7e1032f',
+    VERSION: '0+untagged.1.g4d286ff',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
