c41789cbd6e1144fd03329f466f196cb39c049c4
rmarkello <rmarkello@users.noreply.github.com>
2021-08-16 18:08:55 +0000

    Merge pull request #7 from rmarkello/fix/example
    [FIX] Fix plot_spatial_nulls.py ef229ef6eea2d79b9838bd25aa7e1b7109f4abe4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g75dddf3',
+    VERSION: '0+untagged.1.gef229ef',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
