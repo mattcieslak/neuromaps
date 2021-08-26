c8ed8843165737971eb9fd1985617562c66d462d
rmarkello <rmarkello@users.noreply.github.com>
2021-08-26 15:23:37 +0000

    Merge pull request #9 from rmarkello/contribute
    [MNT] Add issue template forms b1275d65b6d6d145e429efddd4733fbe80ca6c12

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gce2f602',
+    VERSION: '0+untagged.1.gb1275d6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
