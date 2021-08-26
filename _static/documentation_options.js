f37656c6d9caaa069daaba399bbf0fad1ecf09b6
rmarkello <rmarkello@users.noreply.github.com>
2021-08-26 15:29:22 +0000

    [MNT] Update issue template forms f916aabecf735da689a6c33d9468b2d35a59df05

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gb1275d6',
+    VERSION: '0+untagged.1.gf916aab',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
