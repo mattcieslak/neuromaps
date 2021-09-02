9bd89a84713277db2015218111060073cde73d13
rmarkello <rmarkello@users.noreply.github.com>
2021-09-02 17:24:13 +0000

    Merge pull request #13 from rmarkello/osf
    [REF] Fix OSF json and funcs 6ef997e3120a0b3ea40d1e198e249e594f7e17b9

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g9220ede',
+    VERSION: '0+untagged.1.g6ef997e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
