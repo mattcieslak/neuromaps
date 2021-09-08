7d22b8745bcadbddc5d2c973d5591c7d91fb7268
rmarkello <rmarkello@users.noreply.github.com>
2021-09-08 16:17:33 +0000

    Merge pull request #15 from rmarkello/update/docs
    [DOC] Minor doc updates c488cc813b4cda57915edebe8775216390f5c1f6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g6ef997e',
+    VERSION: '0+untagged.1.gc488cc8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
