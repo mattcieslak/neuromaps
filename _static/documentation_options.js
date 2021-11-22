c03d7c26356cce998cda03259408e02e6dbc190e
justinehansen <justinehansen@users.noreply.github.com>
2021-11-22 21:41:03 +0000

    Merge pull request #19 from netneurolab/receptors
    [ENH] Add many receptors 093b44f396f1453fffc89b8f6698a2af6db926bc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gfa526b3',
+    VERSION: '0+untagged.1.g093b44f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
