42151c59091c156f64c4b71c3313c977ad790969
justinehansen <justinehansen@users.noreply.github.com>
2021-12-03 17:55:43 +0000

    [FIX] Update intrinsic timescale 4562a6a2bb5330d98cff92308e42251208c68a68

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g05a74cf',
+    VERSION: '0+untagged.1.g4562a6a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
