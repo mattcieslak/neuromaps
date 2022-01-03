d4e41a378cd420b2e5ba7833af24d318b30b4399
justinehansen <justinehansen@users.noreply.github.com>
2022-01-03 18:58:17 +0000

    Merge pull request #22 from estefanysuarez/documentation
    adding changes to documentation b7654c8d1802b9a1c025047ff87283cba1548a61

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g9c215e7',
+    VERSION: '0+untagged.1.gb7654c8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
