ff75ded50d8b8d0ce2531ab80a1699748a30c5a0
liuzhenqi77 <liuzhenqi77@users.noreply.github.com>
2022-01-06 21:04:12 +0000

    Update README.rst 2035951d3f6367fdc59dbb3895ad5410f9d9e49f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gb0d9394',
+    VERSION: '0+untagged.1.g2035951',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
