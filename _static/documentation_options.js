a85c24dd8bb53dc969aba3b70d4a4c7c335421ac
justinehansen <justinehansen@users.noreply.github.com>
2022-01-12 22:47:21 +0000

    Update README.rst 4e958078f8d02c6655d2843627daafb071d30358

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gb939015',
+    VERSION: '0+untagged.1.g4e95807',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
