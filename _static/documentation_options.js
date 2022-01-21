eef1aecc591c45df1c9bc8bc596a15fdca6ba631
VinceBaz <VinceBaz@users.noreply.github.com>
2022-01-21 18:25:10 +0000

    Merge pull request #32 from VinceBaz/OSF_changes
    [ENH] New versions of MNI1521mm and MNI1522mm ba28fc47e091e86c5dd5466418dbc795106e9c94

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g4e95807',
+    VERSION: '0+untagged.1.gba28fc4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
