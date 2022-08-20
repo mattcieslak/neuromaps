3245289793d82c23907984223d94e42d30229fa6
VinceBaz <VinceBaz@users.noreply.github.com>
2022-08-20 16:39:49 +0000

    Merge pull request #67 from VinceBaz/fix-load_data
    [FIX] Fixed bug in load_data() 849193ae9432652f5ec7da8493bbd6cc8d476fe3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g614a3d5',
+    VERSION: '0+untagged.1.g849193a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
