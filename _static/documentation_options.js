b048359450ddb68b387d87c605c301b13db620e5
VinceBaz <VinceBaz@users.noreply.github.com>
2022-12-08 20:31:18 +0000

    Merge pull request #84 from justinehansen/issue-81
    [ENH] Changes related to Issue #81 d4b3bbf016e824dabaf8663b5b5fa262f49bd52a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g62ba018',
+    VERSION: '0+untagged.1.gd4b3bbf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
