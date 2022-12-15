8e23307f48054e76362e7b20f87d007abfa4d743
VinceBaz <VinceBaz@users.noreply.github.com>
2022-12-15 01:34:17 +0000

    Merge pull request #85 from VinceBaz/fix-docs
    [FIX] Fix wget dns issues in deploy-docs workflow b5c37bd4d3c5b6c39dfe03a9168672928b1c7dfd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gd4b3bbf',
+    VERSION: '0+untagged.1.gb5c37bd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
