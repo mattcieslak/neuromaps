ad46537075b1a77f6fc9bbac8bbfbf0566c6c856
VinceBaz <VinceBaz@users.noreply.github.com>
2023-01-18 19:40:31 +0000

    Merge pull request #86 from VinceBaz/fix-compare_images
    [FIX] Fix nan_policy related bug in compare_images 76ef87315610be13abbc6e1fe3d85afd736c1410

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gb5c37bd',
+    VERSION: '0+untagged.1.g76ef873',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
