4a6b2f95704f2621c0d410ae4912e26d49dc9f12
justinehansen <justinehansen@users.noreply.github.com>
2023-03-03 19:01:34 +0000

    Merge pull request #92 from justinehansen/fslr4k-transforms
    [FIX] Fix medial wall masks for fsLR 4k and 8k surfaces 96ff4f079b3de4bc0a246c76e5c997f0b607f0d8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g47ea75f',
+    VERSION: '0+untagged.1.g96ff4f0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
