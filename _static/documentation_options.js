2a3a508c8d67fd995aa02284d2dea808385a0735
justinehansen <justinehansen@users.noreply.github.com>
2023-04-04 18:05:12 +0000

    Merge pull request #97 from justinehansen/return-nulls
    [ENH] Add option to return nulls in `compare_images()` abf5a5c3d3d011d644b56ea5c6a3953cedd80b37

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gbcdb740',
+    VERSION: '0+untagged.1.gabf5a5c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
